b4ba5f0aa48571a33d99aa5f6f882d9b0b91d8cb
Maltiize <etienne.mart@laposte.net>
2021-12-01 00:49:32 +0100

    test error throwing

--- restaurant-service/src/tests/connectors/fetcherAPI.test.ts
+++ restaurant-service/src/tests/connectors/fetcherAPI.test.ts
@@ -1,14 +1,29 @@
 import { mock } from "jest-mock-extended";
 import fetcherAPI from "../../connectors/fetcherAPI";
-import { AxiosStatic } from "axios";
+import  { AxiosStatic } from "axios";
+
+const mockAxios = mock<AxiosStatic>() as any;
+
+beforeEach(() => {
+    mockAxios.mockClear();
+  });
+  
 
 describe("Fetcher Api Tests", () => {
   test("test getPosts method", async () => {
-    const mockAxios = mock<AxiosStatic>() as any;
     mockAxios.get
       .calledWith("http://image-service:3010/images")
       .mockReturnValue('{"data":"hello"}');
     const api = new fetcherAPI(mockAxios, "http://image-service:3010/images");
     expect(await api.getPosts()).toBe('{"data":"hello"}');
+  });
+
+  test("test getPosts axios not working", async () => {
+    mockAxios.get
+      .mockImplementation(() => {
+        throw new Error("error");
+      });
+    const api = new fetcherAPI(mockAxios, "http://image-service:3010/images");
+    await expect(async () => {await api.getPosts()}).rejects.toThrow();
   });
 });
